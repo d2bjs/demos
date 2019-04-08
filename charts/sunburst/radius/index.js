44fe065d77112c29f8a84e0593cf778c1a9f1d45
Kristiana Rendon <kristiana.rendon@gmail.com>
2019-04-08 12:36:21 -0600

    Updated Index.html, Edited Comments, Actually Added Radius Updated index.html in axis and legend padding. Edited comments (switched slice to arc) in sunburst breadcrumbs. And I didn't actually push the sunburst radius changes in the previous commit, so I added them now.

--- charts/sunburst/radius/index.js
+++ charts/sunburst/radius/index.js
@@ -1,22 +1,28 @@
 // Import stylesheets
 import "./styles.css";
 import { chartSunburst } from "d2b"
-import { select, format } from "d3"
+// scaleOrdinal and schemeAccent is for the color generator for the arcs
+import { select, scaleOrdinal, schemeAccent } from "d3"
 
 // Creating the sunburst object
 var sunburst = chartSunburst();
 
+// You can use different kinds of colorGenerators
+// ex) https://github.com/d3/d3-scale-chromatic
+var color = scaleOrdinal(schemeAccent);
 
-// Div selection from html
+// Div selection from index.html
 // Creating the data for the sunburst chart to be generated
 var chart = select('#chart')
 	.datum({
-		// This is the root of the tree/sunburst chart
-		// All the data below starts off here
+		// The roundness of the corners of each arc
+		cornerRadius: 15,
+		// Inner radius (the inside 'hole/gap')
+		innerRadius: (width, height) => Math.min(width, height) / 5,
+		// Outer radius (the outside ring)
+		outerRadius: (width, height) => Math.min(500, Math.min(width, height) / 1),
 		root: {
-			// The name of the root layer that appears on the chart when hovering over the layer
 			label: "root",
-			// This is all the data in the tree
 			children: [
 				{
 					label: 'child 1',
@@ -68,19 +74,7 @@ var chart = select('#chart')
 				}
 			]
 		},
-		// Breadcrumbs are the expanding horizontal list the appears on
-		// the side of the chart when you hover over the sunburst arcs
-		breadcrumbs: {
-			// Toggles breadcrumb visibility (default is true)
-			enabled: true,
-			// This shows up next to each breadcrumb when you hover over an arc
-			html: function (data, value, percent) { 
-				return `${data.label} ${value} ${percent > 1 ? '' :` (${format('.0%')(percent)}) `}`
-			}, 
-			// Orientation of sunburst
-			orient: "left", 
-		},
-
+		
 	})
 	.call(sunburst.advanced);
 		
