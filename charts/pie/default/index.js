7ab5f60be089590b55972832ddf6c6f5cd669121
Kristiana Rendon <kristiana.rendon@gmail.com>
2019-04-15 10:48:01 -0600

    Added Default Charts (Pie, Donut, Sankey, Sunburst) and Updated Imports Added the default charts (pie, donut, sankey, sunburst) without any customization on the configurations. Simply how they are on d2b.org. Also removed the generator imports from d2b library and added them as types within the generator array in the sets array.

---
+++ charts/pie/default/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import { select } from "d3";
+import { chartPie } from "d2b";
+
+var pie = chartPie();
+
+var chart = select('#chart')
+	.datum([
+		{label: 'arc 1', value: 23},
+		{label: 'arc 2', value: 31},
+		{label: 'arc 3', value: 80},
+		{label: 'arc 4', value: 8}
+	])
+	.call(pie.advanced);
+
+window.addEventListener('resize', function(){
+	chart.call(pie.advanced);
+});
---
+++ charts/sunburst/default/index.js
@@ -0,0 +1,73 @@
+import "./styles.css";
+import { chartSunburst } from "d2b"
+import { select } from "d3"
+
+var sunburst = chartSunburst();
+
+sunburst.chartFrame().size({height: 500});
+
+var chart = select('.sunburst-chart');
+
+chart
+	.datum({
+	label: 'root',
+	children: [
+		{
+		label: 'child 1',
+		children: [
+			{
+			label: 'child 1-1',
+			size: 10
+			},
+			{
+			label: 'child 1-2',
+			children: [
+				{
+				label: 'child 1-2-1',
+				size: 5
+				},
+				{
+				label: 'child 1-3-1',
+				size: 8
+				}
+			]
+			},
+			{
+			label: 'child 1-3',
+			// selected: true,
+			children: [
+				{
+				label: 'child 1-3-1',
+				children: [
+					{
+					label: 'child 1-3-1-1',
+					size: 2
+					},
+					{
+					label: 'child 1-3-1-2',
+					size: 16
+					}
+				]
+				},
+				{
+				label: 'child 1-3-2',
+				size: 8
+				}
+			]
+			}
+		]
+		},
+		{
+		label: 'child 2',
+		size: 25
+		}
+	]
+	});
+	  
+update();
+
+function update () {
+	chart.call(sunburst.advanced);
+}
+
+window.addEventListener('resize', update);
