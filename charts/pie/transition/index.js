d64ec910d1e079fa80f6b1cb966622cb675558dc
Kristiana Rendon <kristiana.rendon@gmail.com>
2019-05-09 13:54:43 -0600

    Edited Title Names in HTML/JSON, Added New Pie Data Transition Chart Edited the title names in the HTML/JSON so it will display the correct title for its chart. Added a new pie data transition chart. This has a button, so when it is clicked, it will transition the chart to new data.

---
+++ charts/pie/transition/index.js
@@ -0,0 +1,57 @@
+import "./styles.css";
+import { select } from "d3";
+import { chartPie } from "d2b";
+
+const click = document.getElementById("transition");
+click.addEventListener("click", function() {
+	changeChart();
+});
+
+const pie = chartPie();
+
+const chart = select('#chart')
+	.datum([
+		{label: 'arc 1', value: 13},
+		{label: 'arc 2', value: 61},
+		{label: 'arc 3', value: 30},
+		{label: 'arc 5', value: 80}
+	])
+	.call(pie.advanced);
+var transition = false;
+
+function changeChart() {
+	if(transition) 
+	{
+		chart
+		.datum([
+            {label: 'arc 1', value: 13},
+            {label: 'arc 2', value: 61},
+            {label: 'arc 3', value: 30},
+            {label: 'arc 5', value: 80}
+		])
+		.transition()
+		.duration(2000)
+		.call(pie.advanced);	
+		transition = false;
+	}
+	else 
+	{
+		chart
+		.datum([
+			{label: 'arc 1', value: 23},
+			{label: 'arc 2', value: 31},
+			{label: 'arc 3', value: 80},
+			{label: 'arc 4', value: 8}
+		])
+		.transition()
+		.duration(2000)
+		.call(pie.advanced);	
+		transition = true;
+	}
+
+};
+
+
+window.addEventListener('resize', function(){
+	chart.call(pie.advanced);
+});
